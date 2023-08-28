6bb71bf21888ca1eefd1f37674a39e748f439d49
ricardo <ricardo@in8.com.br>
2023-08-28 18:39:07 -0300

    Make connection with DB

--- Agenda/src/main/java/controller/Controller.java
+++ Agenda/src/main/java/controller/Controller.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO;
 
 /**
  * Servlet implementation class Controller
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DAO dao = new DAO();
        
     /**
      * @see HttpServlet#HttpServlet()
